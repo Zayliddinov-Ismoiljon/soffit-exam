86c87455c4a5bf9758169036be996c8db81dae8d
Zayliddinov-Ismoiljon <zayliddinovismoiljon@gmail.com>
2022-03-06 23:53:00 +0500

    three commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
+var a=false;
+
+
 function openFunction(){
     document.getElementById("menu").style.width="100%";
+    if(a){
+        document.getElementById("menu").style.width="0px";
+        a=false;
+    }
+    else{
+        document.getElementById("menu").style.width="100%";
+        a=true;
+    }
 }
 
 function closeFunction(){
